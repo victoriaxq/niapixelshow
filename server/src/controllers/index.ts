53e0bc598e8276c44950044c46dd6835289b9aa2
Victória <victoriaxavierq@gmail.com>
2024-06-25 22:38:21 -0300

    feat(dev): removing category controller

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -4,16 +4,14 @@ import OrderController from './OrderController';
 import PromotionController from './PromotionController';
 import AuthController from './AuthController';
 import ProductController from './ProductController';
-<<<<<<< HEAD
-=======
-import CategoryController from './CategoryController';
->>>>>>> 5e1587c706313e69fcc451a84b7bd3fc86d8a285
 
 export {
     UserController,
     AuthController,
     ProductController,
-    PromotionController
+    PromotionController,
+    CartController,
+    OrderController
 };
 
 
